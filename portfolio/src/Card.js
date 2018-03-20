fd544dba37dd2432f309308d1546114dcef45e62
Nathan Simmons <nathanhbsimmons@gmail.com>
2018-03-20 15:55:32 -0500

    created projects obj, added card map

--- portfolio/src/Card.js
+++ portfolio/src/Card.js
@@ -1,45 +1,43 @@
 import React, { Component } from 'react';
+import './App.css';
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
 import logo from './logo.png';
+
+
+const styles = {
+  cardStyle: {
+    width: '260px',
+    height: '260px',
+  },
+  overlayStyle: {
+    width: '260px',
+    height: '260px',
+  }
+}
+
+
 class ProjectCard extends Component {
 
-
   render () {
-    const cardStyle = {
-      width: '300px',
-      height: '300px'
-    }
-    const overlayStyle ={
-      height: '268px'
-    }
+
     return (
-
-      <Card style={cardStyle}>
-        {/* <CardHeader
-          title="URL Avatar"
-          subtitle="Subtitle"
-          avatar="images/jsa-128.jpg"
-        /> */}
-        <CardMedia
-
-            overlay={<CardTitle  style={overlayStyle} title="Overlay title" />}
-          >
-            <img src={logo} alt="" />
-          </CardMedia>
-          {/* <CardTitle title="Card title" subtitle="Card subtitle" /> */}
-          {/* <CardText>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-            Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-            Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
-          </CardText>
-          <CardActions>
-            <FlatButton label="Action1" />
-            <FlatButton label="Action2" />
-          </CardActions> */}
-        </Card>
-        
+      <div>
+        {this.props.projects.map((projectObj, i)=>{
+          return (<div style={styles.cardStyle}>
+            <a href={projectObj.url} target="_blank">
+              <article className="panel">
+                <img src={projectObj.img} style={styles.overlayStyle}/>
+                <section className="info">
+                  {projectObj.title}
+                </section>
+                <section className="filter">
+                </section>
+              </article>
+            </a>
+          </div>)
+        })}
+      </div>
     )
   }
 
--- portfolio/src/Nav.js
+++ portfolio/src/Nav.js
@@ -1,81 +1,94 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {Tabs, Tab} from 'material-ui/Tabs';
 import SwipeableViews from 'react-swipeable-views';
 import ProjectCard from './Card.js'
+import restPic from './restdemo2.png';
 
 const styles = {
   headline: {
     fontSize: 24,
     paddingTop: 16,
     marginBottom: 12,
-    fontWeight: 400,
+    fontWeight: 400
   },
   slide: {
-    padding: 10,
+    padding: 10
   },
   header: {
+
     width: '90%',
-    margin: 'auto'
+    margin: 'auto',
+
   },
-  container: {
+  border: {
+    height: 46,
+    width: '20%',
+    borderBottom: '2px solid #EF5743',
+    background: 'ghostwhite'
+  },
+  bodyContainer: {
     padding: 10,
     width: '90%',
     margin: 'auto',
     display: 'flex',
-    justifyContent: 'spaceBetween',
-
+    justifyContent: 'spaceBetween'
+  },
+  navContainer: {
+    display: 'flex',
+  },
+  navTabs: {
+    background: 'ghostwhite',
+    color: '#02BEC4',
   }
 };
 
 class Nav extends Component {
   constructor(props) {
-  super(props);
-  this.state = {
-    slideIndex: 0,
+    super(props);
+    this.state = {
+      projects: [
+        {title: 'Rest Demo',
+          url: 'https://nathanhbsimmons.github.io/checkpoints/cpoint1/cpoint1.html',
+          img: restPic},
+      ],
+      slideIndex: 0
+    };
+  }
+
+  handleChange = (value) => {
+    this.setState({slideIndex: value});
   };
-}
 
-handleChange = (value) => {
-  this.setState({
-    slideIndex: value,
-  });
-};
+  render() {
+    return (<div>
+      <div style={styles.navContainer}>
+      <div style={styles.border}></div>
+      <Tabs style={styles.header} onChange={this.handleChange} value={this.state.slideIndex}>
 
-render() {
-  return (
-    <div>
-      <Tabs
-        style={styles.header}
-        onChange={this.handleChange}
-        value={this.state.slideIndex}
-      >
-        <Tab label="Projects" value={0} />
-        <Tab label="About" value={1} />
-        <Tab label="Contact" value={2} />
+        <Tab style={styles.navTabs} label="Projects" value={0}/>
+        <Tab style={styles.navTabs} label="About" value={1}/>
+        <Tab style={styles.navTabs} label="Contact" value={2}/>
+
       </Tabs>
-      <SwipeableViews
-        index={this.state.slideIndex}
-        onChangeIndex={this.handleChange}
-      >
-        <div style={styles.container}>
+      <div style={styles.border}></div>
+      </div>
 
-          <ProjectCard/>
-          <ProjectCard/>
-          <ProjectCard/>
-        </div>
-        <div style={styles.container}>
-          <p>Nathan Simmons has finally found the outlet he needs to channel his visceral desire to succeed, relentless problem solving skills, passion for creating and general left/right brain cohesion. After years spent in restaurant management and playing music, he is coupling his creativity and artist eye with his experience in daily business operations, people management skills and networking. Powerful and diverse experience, combined with full stack javascript web development training, come together to create a new and unstoppable force in the development world.</p>
-        </div>
-        <div style={styles.slide}>
-          slide n°3
-        </div>
-      </SwipeableViews>
-    </div>
-  );
+        <SwipeableViews index={this.state.slideIndex} onChangeIndex={this.handleChange}>
+          <div style={styles.bodyContainer}>
+            <ProjectCard projects={this.state.projects}/>
+
+          </div>
+          <div style={styles.bodyContainer}>
+            <p>Nathan Simmons has finally found the outlet he needs to channel his visceral desire to succeed, relentless problem solving skills, passion for creating and general left/right brain cohesion. After years spent in restaurant management and playing music, he is coupling his creativity and artist eye with his experience in daily business operations, people management skills and networking. Powerful and diverse experience, combined with full stack javascript web development training, come together to create a new and unstoppable force in the development world.</p>
+          </div>
+          <div style={styles.slide}>
+            slide n°3
+          </div>
+        </SwipeableViews>
+
+
+    </div>);
+  }
 }
-}
-
-
-
 
 export default Nav;
