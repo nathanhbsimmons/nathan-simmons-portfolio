802f83a21bf275290b1318ec11c0e7c588ee4d4f
Nathan Simmons <nathanhbsimmons@gmail.com>
2018-08-20 11:09:41 -0500

    changed description

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
           img: currents
         }, 
         {
-          title: 'SCROGGLE (Google Firebase)',
+          title: 'SCROGGLE Words (Google Firebase)',
           url: 'https://words-game-challenge.firebaseapp.com/',
           img: scroggle
         },
